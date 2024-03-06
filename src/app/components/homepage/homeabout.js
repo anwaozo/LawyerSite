622984745ea9a5cd0faa51132d02a98150caf3dd
trinathutl <trinath@utahtechlabs.com>
2024-03-07 00:58:53 +0530

    responsive

--- src/app/components/homepage/homeabout.js
+++ src/app/components/homepage/homeabout.js
@@ -1,10 +1,45 @@
+"use client";
 import Image from "next/image";
-import React from "react";
+import React, { useEffect } from "react";
 import dynamic from "next/dynamic";
 import { legalTeamSection } from "../../service-cms/homePage";
 
 export default function HomeaboutComponent() {
   console.log(legalTeamSection);
+  useEffect(() => {
+    var a = 0;
+    if(window){
+      $(window).scroll(function () {
+        var oTop = $("#counter").offset().top - window.innerHeight;
+        if (a == 0 && $(window).scrollTop() > oTop) {
+          $(".counter-value").each(function () {
+            var $this = $(this),
+              countTo = $this.attr("data-count");
+            $({
+              countNum: $this.text(),
+            }).animate(
+              {
+                countNum: countTo,
+              },
+  
+              {
+                duration: 3000,
+                easing: "swing",
+                step: function () {
+                  $this.text(Math.floor(this.countNum));
+                },
+                complete: function () {
+                  $this.text(this.countNum);
+                },
+              }
+            );
+          });
+          a = 1;
+        }
+      });
+    }
+    
+  }, []);
   const { title, subHeading, sectionLink, sectionInfo } = legalTeamSection;
   return (
     <section className="about-area">
@@ -42,11 +77,14 @@ export default function HomeaboutComponent() {
                 <div
                   key={info?.title}
                   className="about-info-item"
+                  id="counter"
                   data-aos="zoom-in"
                   data-aos-delay="200"
                   data-aos-duration="1500"
                 >
-                  <h6>{info?.displayedNumber}</h6>
+                  <h6 class="counter-value" data-count={info?.displayedNumber}>
+                    0
+                  </h6>
                   <p>{info?.title}</p>
                 </div>
               ))}
